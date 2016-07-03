d54d54f40cdc62e3d2d0bf6221ee283abdebe731
Gui Talarico <gtalarico@gmail.com>
2016-07-03 02:23:26 -0400

    Some generic Clean up and test setup. Nothing in empty

--- tests/tests.py
+++ tests/tests.py
@@ -1,34 +1,18 @@
 import unittest
-from foldify.path_tree import PathO, tree_from_folder, tree_from_json_dict
+from foldify.tree import Tree, Node, get_type, read_path
+
+# from foldify python -m unittest discover -s ..
 
 TEST_FOLDER = 'root'
 
 # TODO:
 # Rewrite all tests!
 
-class TestPathTreeClass(unittest.TestCase):
+class TestNode(unittest.TestCase):
 
     def setUp(self):
-        root = PathO('rootpath')
-        child1 = PathO('child1')
-        child2 = PathO('child2')
-        root.add_child(child1)
-        root.add_child(child2)
-        self.tree = root
+        self.tree = Tree('root.json')
 
-    def test_tree(self):
-        assert isinstance(self.tree, PathO)
-        assert self.tree.root.name == 'rootpath'
-        assert len(self.tree) == 2
-
-    def test_children(self):
-        # import pdb; pdb.set_trace()
-        child1 = self.tree.children[0]
-        assert isinstance(child1, PathO)
-        assert len(child1) == 0
-        assert child1.name == 'child1'
-        assert child1.root.name == 'rootpath'
-        assert child1.parent.name == 'rootpath'
 
 
         # with self.assertRaises(TypeError):
