42a6a5f72b6858ae72d1705007b07c4bdff17aae
Gui Talarico <gtalarico@gmail.com>
2016-07-03 21:07:30 -0400

    Added setup.py for distribution

--- foldify/__init__.py
+++ foldify/__init__.py
@@ -1,7 +1,7 @@
 'Foldify - CLI tools for managing directory Trees.'
 
 __author__ = 'gtalarico@gmail.com'
-__version__ = '3.2'
+__version__ = '0.3.3'
 
 '''
 Release Notes
---
+++ setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+setup(
+  name = 'foldify',
+  packages = ['foldify'], # this must be the same as the name above
+  scripts=['foldify/foldify']
+  version = '0.3.3',
+  description = 'Python CLI tools to help manage directory trees and templates',
+  author = 'Gui Talarico',
+  author_email = 'gtalarico@gmail.com',
+  url = 'https://github.com/gtalarico/foldify', # use the URL to the github repo
+  license = 'MIT',
+  download_url = 'https://github.com/gtalarico/foldify/archive/0.3.3.tar.gz', # I'll explain this in a second
+  keywords = ['folders', 'directories'], # arbitrary keywords
+  classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.6',
+        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
