78706e88e113e47394e7754a99aa33d0df19104c
Gui Talarico <gtalarico@gmail.com>
2016-07-03 21:18:04 -0400

    Setup tools fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'foldify',
   packages = ['foldify'], # this must be the same as the name above
-  scripts=['foldify/foldify']
+  scripts=['foldify/foldify'],
   version = '0.3.3',
   description = 'Python CLI tools to help manage directory trees and templates',
   author = 'Gui Talarico',
